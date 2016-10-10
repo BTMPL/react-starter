e8a1c5d5f762597a0750c072c9c7c8e3177a2d32
Bartosz Szczecinski <bartosz.szczecinski@gft.com>
2016-10-10 11:39:15 +0200

    hello world in React using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@ const webpack = require("webpack");
 module.exports = {
   devtool: 'source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
-    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     './src/index'
   ],
   output: {
