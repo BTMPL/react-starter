73a50a25eff98802e8db9ee5d605e7e627e74444
Bartosz Szczecinski <bartosz.szczecinski@gft.com>
2016-10-10 11:41:03 +0200

    fixed an error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 export default class App extends Component {
   render() {
