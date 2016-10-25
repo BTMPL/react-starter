6def19b9f45bcd424c16ac93d677f177c66926f5
Bartosz Szczecinski <bartosz.szczecinski@gft.com>
2016-10-25 13:26:26 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import Pane from "./Pane";
-import Tab from "./Tab";
-import Box from "./Box";
-
-export default class App extends Component {
-
-  getChildContext() {
-    return {
-      language: {
-        hello: "Hello"
-      }
-    };
-  }
-  render() {
-    return (
-      <Pane>
-        <Tab>
-          <Box userName="Bartosz" />
-        </Tab>
-      </Pane>
-    );
-  }
-}
-
-App.childContextTypes = {
-  language: React.PropTypes.Object
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  React.createElement('div', null, 'Hello! World!'),
+  document.getElementById('root')
+);
