4fd55d51368b39bf36850e5c3fbfcd26996228d8
Bartosz Szczecinski <bartosz.szczecinski@gft.com>
2016-10-25 15:16:54 +0200

    some work done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component {
+class Button extends Component {
+
   render() {
     return (
-      <h1>Hello world!</h1>
+      <button style={this.props.style}>
+        {this.props.emoticon}
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+
+class Emoticon extends Component {
+
+  render() {
+    return (
+      <span>:)</span>
+    );
+  }
+}
+
+class ChangeName extends Component {
+
+  handleClick() {
+    this.props.onClick();
+  }
+
+  render() {
+    return (
+      <button onClick={() => {
+          this.handleClick();
+        }} >Change my name!</button>
+    );
+  }
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Bratosz"
+    };
+
+    this.handleClick = this.handlClick.bind(this);
+  }
+
+  handleClick() {
+      this.setState({
+        name: "Nah, another name!"
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.name}
+        <ChangeName
+          onClick={this.handleClick} />
+      </div>
     );
   }
 }
