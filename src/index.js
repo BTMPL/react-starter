0cb4ecfda89a58e07df49b6427f3dcafa61850f8
Bartosz Szczecinski <bartosz.szczecinski@gft.com>
2016-10-26 06:57:52 +0200

    redux example

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+
+const ACTIONS = {
+  increase: "counter/increase"
+};
+
+function counterReducer (state = {count: 0}, action) {
+  switch(action.type) {
+    case ACTIONS.increase:
+      return {count: state.count + 1}
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counterReducer);
+
+
+class Button extends React.Component {
+
+  render() {
+    return (
+      <button onClick={() => store.dispatch({type: ACTIONS.increase})}>{this.props.children}</button>
+    )
+  }
+}
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Button>This button</Button> was clicked { this.props.count } times!
+      </div>
+    );
+  }
+};
+
+const ConnectedApp = connect(() => ({
+  count: store.getState().count
+}))(App);
+
+
+render(
+  <Provider store={store}>
+    <ConnectedApp />
+  </Provider>,
+document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
+
+
+module.exports = {
+  entry: [
+    "./src/index"
+  ],
+  output: {
+    filename: "bundle.js",
+    // eslint-disable-next-line
+    path: __dirname + "/dist"
+  },
+  plugins: [
+    new webpack.ResolverPlugin(new DirectoryNamedWebpackPlugin()),
+    new webpack.DefinePlugin({
+      // eslint-disable-next-line
+      NODE_ENV: '"' + process.env.NODE_ENV + '"'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js/,
+        loaders: ["babel"],
+        exclude: /(node_modules)/
+      }
+    ]
+  }
+};
